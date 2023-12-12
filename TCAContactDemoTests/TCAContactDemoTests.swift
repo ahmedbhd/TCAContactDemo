923723a4ca9f5936c4f11e48ac8ab3abb1bdce2e
Ahmed Ben Henda <ahmed.bnhenda@gmail.com>
2023-12-12 09:09:54 +0100

    Test presentation section 2 done

--- TCAContactDemoTests/TCAContactDemoTests.swift
+++ TCAContactDemoTests/TCAContactDemoTests.swift
@@ -47,4 +47,48 @@ final class TCAContactDemoTests: XCTestCase {
         }
     }
     
+    func testAddFlow_NonExhaustive() async throws {
+        let store = TestStore(initialState: ContactsFeature.State()) {
+            ContactsFeature()
+        } withDependencies: {
+            $0.uuid = .incrementing
+        }
+        store.exhaustivity = .off
+        
+        await store.send(.addButtonTapped)
+        await store.send(.destination(.presented(.addContact(.setName("Blob Jr.")))))
+        await store.send(.destination(.presented(.addContact(.saveButtonTapped))))
+        await store.skipReceivedActions()
+        store.assert {
+            $0.contacts = [
+                Contact(id: UUID(0), name: "Blob Jr.")
+            ]
+            $0.destination = nil
+        }
+    }
+
+    func testDeleteContact() async throws {
+        let store = TestStore(
+            initialState: ContactsFeature.State(
+                contacts: [
+                    Contact(id: UUID(0), name: "Blob"),
+                    Contact(id: UUID(1), name: "Blob Jr."),
+                ]
+            )
+        ) {
+            ContactsFeature()
+        }
+        
+        await store.send(.deleteButtonTapped(id: UUID(1))) {
+            $0.destination = .alert(
+                AlertState {
+                    TextState("Are you sure?")
+                } actions: {
+                    ButtonState(role: .destructive, action: .confirmDeletion(id: UUID(1))) {
+                        TextState("Delete")
+                    }
+                }
+            )
+        }
+    }
 }
